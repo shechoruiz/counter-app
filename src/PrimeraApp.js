8b3d4f7037830c62671144bfccca15be02b7c588
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-07 22:58:28 -0500

    Hola mundo - Creación de componentes - Retorno de elementos - Impresión de variables

---
+++ src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+  const salud = 'Hola mundo'
+
+  return (
+    <>
+      <h1>{salud}</h1>
+      <h2>Mi primera aplicación</h2>
+    </>
+  );
+};
+
+export default PrimeraApp;
