c3a74dd6b1f970ce348a16f4994e4a48cf48aab9
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-08 23:42:39 -0500

    Aplicación completa con useState - CounterApp

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 import './index.css'
-import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector('#root')
 
-ReactDOM.render( <PrimeraApp />, divRoot )
+ReactDOM.render( <CounterApp value={10} />, divRoot )
