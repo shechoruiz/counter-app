8463609bceceaf5087eca714e8aca2a0328682d3
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-09 22:04:56 -0500

    1ros pasos en pruebas con Jest

---
+++ src/tests/demo.test.js
@@ -0,0 +1,11 @@
+
+describe('Pruebas en el archivo demo.test.js', () => {
+  test('1. Deben de ser iguales los 2 strings', () => {
+    // 1. Inicialización
+    const mensaje = 'Hola mundo'
+    // 2. Estimulo
+    const mensaje2 = `Hola mundo`
+    // 3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2)
+  })
+})
